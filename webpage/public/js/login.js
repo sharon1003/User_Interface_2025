968a5d013975394f9614b2a32a8ecad9e613c265
dndotter <niedanning@live.com>
2025-03-16 14:57:35 +0100

    imported userdata from json

--- webpage/public/js/login.js
+++ webpage/public/js/login.js
@@ -1,35 +1,39 @@
+import userDatabase from "../data/UserDB.json" with { type : "json"};
+
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault(); // 阻止默认提交
 
     // const role = document.getElementById('role').value;
-    const userDatabase = {
+    /*const userDatabase = {
     "alice": { password: "123456", role: "customer" },
     "bob": { password: "vip123", role: "vip" },
     "charlie": { password: "staff456", role: "staff" },
     "admin": { password: "admin789", role: "owner" }
-    };
+    };*/
 
     const username = document.getElementById('username').value.trim().toLowerCase(); // 统一小写
     const password = document.getElementById('password').value.trim();
     const loginMessage = document.getElementById('loginMessage');
 
     if (!username || !password) {
+        //console.log("aaa");
+        
         loginMessage.innerText = "Please enter both username and password.";
         return;
     }
 
-    // 校验用户名是否存在
+    // if user exists
     if (userDatabase.hasOwnProperty(username)) {
-        // 校验密码
+        // if password correct
         if (userDatabase[username].password === password) {
             const role = userDatabase[username].role;
             sessionStorage.setItem('username', username);
             sessionStorage.setItem('role', role);
 
-            // 根据角色跳转
+            // redirect to role pages
             const rolePageMap = {
                 "customer": "menu.html",
-                "vip": "vip-menu.html",
+                "vip": "vip/vip-menu.html",
                 "staff": "bartender.html",
                 "owner": "owner.html"
             };
