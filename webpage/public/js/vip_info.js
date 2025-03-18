dda0373b768707d91f80bb5c2e1efbbed090609f
dndotter <niedanning@live.com>
2025-03-18 11:06:32 +0100

    Vip Info : TODO loadOrders

--- webpage/public/js/vip_info.js
+++ webpage/public/js/vip_info.js
@@ -1,6 +1,11 @@
 import UserModel from "./user_model.js";
+import {loadOrders} from "./orders_model.js";
+
 const person = UserModel.getLoggedInUser();
+const orders = loadOrders();
 
 document.getElementById("BalNum").innerHTML = person.balance;
 
 document.getElementById("Greeting").innerHTML = person.username;
+
+document.getElementById("OrderHistory").innerText = orders;
