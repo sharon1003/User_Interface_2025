85e1e69d10dec05636c4a98d5db209c2d22f55aa
dndotter <niedanning@live.com>
2025-03-19 00:05:23 +0100

    format refine

--- webpage/public/js/vip_controller.js
+++ webpage/public/js/vip_controller.js
@@ -1,15 +1,15 @@
-document.getElementById("code-popup");
+/*document.getElementById("code-popup");
 
 window.onclick = function(event) {
     /*if (event.target == modal) {
         modal.style.display = "none";
-    }*/
-}
+    }
+}*/
 
 const button = document.getElementById("code-btn");
 
 function codeGenerator () {
-    document.getElementById("code-popup").innerHTML = 
+    document.getElementById("code-btn").innerHTML = 
     "The code of fridge is " + 
     `<b>${Math.floor(Math.random() * 10)}</b>` +
     `<b>${Math.floor(Math.random() * 10)}</b>` +
@@ -17,3 +17,4 @@ function codeGenerator () {
     `<b>${Math.floor(Math.random() * 10)}</b>`;
 }
 button.onclick = codeGenerator;
+console.log("123");
